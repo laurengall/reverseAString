32a623fc30ff59f3d86e1e15188c9b95f7224739
Lauren Gallagher <lauren.21.gallagher@gmail.com>
2023-02-21 06:49:12 -0500

    commit

--- Main (7).java
+++ Main (7).java
@@ -1,17 +1,21 @@
-import java.util.Scanner; 
+import java.util.Scanner;
+
 class Main {
   public static void main(String[] args) {
-   Scanner input = new Scanner(System.in); 
-    System.out.println("Enter a word:"); 
-    String str = input.nextLine(); 
-    reverse(str); 
+    Scanner input = new Scanner(System.in);
+    System.out.println("What operation would you like to perform?\n1. Reverse a String\n2. Remove spaces");
+    int num = input.nextInt();
+    System.out.println("Enter a word:");
+    String str = input.nextLine();
+
   }
-  public static void reverse (String str) {
-    String newStr = ""; 
-    for (int x = str.length() - 1; x > -1; x--){
-   newStr += str.charAt(x); 
+
+  public static void reverse(String str) {
+    String newStr = "";
+    for (int x = str.length() - 1; x > -1; x--) {
+      newStr += str.charAt(x);
     }
-    System.out.println(newStr); 
+    System.out.println(newStr);
   }
-  
+
 }
