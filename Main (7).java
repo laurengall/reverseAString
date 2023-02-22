fded7f1749e5b67d2791b5fa8552d8957bef7292
Lauren Gallagher <lauren.21.gallagher@gmail.com>
2023-02-22 14:07:45 -0500

    commit

--- Main (7).java
+++ Main (7).java
@@ -5,12 +5,22 @@ class Main {
     Scanner input = new Scanner(System.in);
     System.out.println("What operation would you like to perform?\n1. Reverse a String\n2. Remove spaces");
     int num = input.nextInt();
-    System.out.println("Enter a word:");
-    String str = input.nextLine();
+
+    if (num == 1) {
+
+      reverse(str);
+    } else if (num == 2) {
+      System.out.println("Enter a word:");
+      String str = input.nextLine();
+      noSpace(str);
+    } else {
+
+    }
 
   }
 
   public static void reverse(String str) {
+
     String newStr = "";
     for (int x = str.length() - 1; x > -1; x--) {
       newStr += str.charAt(x);
@@ -18,4 +28,11 @@ class Main {
     System.out.println(newStr);
   }
 
+  public static void noSpace(String str) {
+    String aStr = "";
+    for (int x = 0; x < str.length(); x++) {
+      str.replaceAll("\\s", "");
+    }
+    System.out.println(aStr);
+  }
 }
