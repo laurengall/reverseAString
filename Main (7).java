f5b8561edfd8281dbd1efc76eadaa3e54aee4c97
Lauren Gallagher <lauren.21.gallagher@gmail.com>
2023-02-28 18:10:14 -0500

    commit

--- Main (7).java
+++ Main (7).java
@@ -19,6 +19,8 @@ class Main {
         lowerCase(str);
       } else if (num == 5) {
         concat(str);
+      } else if (num == 6) {
+        palindrome(str);
       }
       System.out.println("Would you like to do another manipulation? Type 1 for yes and 2 for no.");
       int x = input.nextInt();
@@ -65,6 +67,16 @@ class Main {
     System.out.println(str + " concatenated with " + add + " is " + newStr);
   }
 
+  public static void palindrome(String str) {
+    String check = str;
+    reverse(check);
+    if (str.equals(check)) {
+      System.out.println("Yes, this string is palindrome.");
+    } else {
+      System.out.println("No, this string is not a palindrome.");
+    }
+  }
+
   public static void print() {
     System.out.println("Type the number of the operation you would like to perform.");
     System.out.println("1. Reverse a string.");
@@ -72,5 +84,6 @@ class Main {
     System.out.println("3. Print in all uppercase letters.");
     System.out.println("4. Print in all lowercase letters.");
     System.out.println("5. Concatenate two strings.");
+    System.out.println("6. Check if string is a palindrome.");
   }
 }
